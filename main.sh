88e6d3c7640df12919e01eadf8c2384cde1961fc
SebastienVProject <vergnollesebastien@icloud.com>
2017-05-01 11:05:15 +0200

    Enrichir une liste de courses à partir d'un code barre et d'une quantité

---
+++ main.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./update_liste.sh 3274080005003 4
+./update_liste.sh 3179732370754 6
+./update_liste.sh 3274080005003 -2
+./update_liste.sh 3274080005003 -3
+
+exit 0
---
+++ update_liste.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]
+then
+	echo "[ERROR] Nombre d'arguments incorrects"
+	exit 12
+fi
+
+codeBarre=$1
+quantite=$2
+
+clear
+# Récupération d'informations produit à partir d'un code barre
+fichierCourses="./listeDeCourses.txt"
+
+#Utilisation API permettant de récupérer des infos produit dans une base communautaire
+infoProduit=`curl http://world.openfoodfacts.org/api/v0/product/${codeBarre}.json`
+
+#On teste si un produit a été trouvé ou nom dans l'api
+statusVerbose=`echo $infoProduit | jq '.status_verbose'`
+if [ "<$statusVerbose>" = '<"product not found">' ]
+then
+	produitTrouve=false
+else
+	produitTrouve=true
+fi
+
+#Actions à exécuter si produit trouvé
+if $produitTrouve 
+then
+	ingredients=`echo $infoProduit | jq '.product.ingredients_text_with_allergens'`
+	libelleProduit=`echo $infoProduit | jq '.product.generic_name_fr'`
+	imageProduit=`echo $infoProduit | jq '.product.image_ingredient_small_url'`
+	brands=`echo $infoProduit | jq '.product.brands'`
+
+
+	echo $ingredients
+	echo $libelleProduit
+	echo $imageProduit
+	echo $brands
+fi
+
+#Ajout du produit dans la liste de courses
+enregistrement=`grep $codeBarre $fichierCourses`
+if [ "$enregistrement" = ""  ]
+then
+	if [ $quantite -gt 0 ]
+	then
+		echo "${codeBarre}|${quantite}|${libelleProduit}|${brands}|${ingredients}|${imageProduit}" >> $fichierCourses
+	fi
+else
+	quantitePrecedente=`echo $enregistrement | cut -d'|' -f2`
+	sed -i "/^${codeBarre}/d" $fichierCourses
+	quantite=`expr $quantite + $quantitePrecedente`
+	if [ $quantite -gt 0 ]
+	then
+		echo "${codeBarre}|${quantite}|${libelleProduit}|${brands}|${ingredients}|${imageProduit}" >> $fichierCourses
+	fi
+fi
+
+vim ./listeDeCourses.txt
+exit 0
